ebc38c36483f1f43816da6e7c8bee5a883b81c6e
GuillermoFQP <gfq@sfu.ca>
2025-08-14 00:18:11 -0700

    Modify parameters

--- multifield_globals.f90
+++ multifield_globals.f90
@@ -3,8 +3,8 @@ module multifield_globals
 implicit none
 
 real, parameter             :: pi = 4.0 * atan(1.0)                                 ! Constant
-character(len=*), parameter :: field_space_geometry = "Renaux-Petel"                ! Choose between "Euclidean", "Renaux-Petel" or "alpha-attractor"
-character(len=*), parameter :: potential_shape = "non-linear"                       ! Choose between "elliptic", "non-linear" or "hybrid"
+character(len=*), parameter :: field_space_geometry = "Euclidean"                   ! Choose between "Euclidean", "Renaux-Petel" or "alpha-attractor"
+character(len=*), parameter :: potential_shape = "hybrid"                           ! Choose between "elliptic", "non-linear" or "hybrid"
 real, parameter             :: massfield1 = 1.4d-6, massfield2 = 7.0 * massfield1   ! Elliptic potential parameters
 real, parameter             :: lambda_cons = 1.d-14, g_cons = 2.d-14, mu_cons = 0.0 ! Non-linear potential parameters
 real, parameter             :: lambda_hp = 1.0d-12, m1_hp = 1.95d-6                 ! Hybrid inflation potential parameters
