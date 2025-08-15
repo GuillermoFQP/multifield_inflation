bdfae27e974a0b7075301f50ec14757f2c4f21b2
GuillermoFQP <gfq@sfu.ca>
2025-08-15 16:42:52 -0700

    Add grid of initial conditions and modify loop condition

--- bkgd_trajs.f90
+++ bkgd_trajs.f90
@@ -12,8 +12,8 @@ implicit none
 
 real, parameter    :: N_bound = 500.0                     ! Upper bound in N
 real, parameter    :: dN = 0.01                           ! Data flushing period
-real, parameter    :: dt = 10.0                           ! Time-step
-integer, parameter :: ntrajs = 10                         ! Number of trajectories per horizontal side of the grid
+real, parameter    :: dt = 500.0                          ! Time-step
+integer, parameter :: ntrajs = 20                         ! Size of the grid of initial conditions
 real               :: traj(int(1 + N_bound/dN), 2*ntrajs) ! Trajectories array
 real, dimension(6) :: y                                   ! State array
 real, dimension(2) :: phi, phidot, phi_min, phi_max, dphi ! Colormap grid parameters
@@ -32,14 +32,14 @@ end if
 call execute_command_line('rm -rf trajs; mkdir trajs')
 
 ! Define the range for initial conditions
-phi_min = [-20.0, -1.0]
-phi_max = [ 20.0,  1.0]
+phi_min = [-20.0, -20.0]
+phi_max = [ 20.0,  20.0]
 
 ! Define the step size (NGRID-1 intervals between NGRID points)
-dphi = [(phi_max(1) - phi_min(1)) / real(ntrajs-1),  phi_max(2) - phi_min(2)]
+dphi = [(phi_max(1) - phi_min(1)) / real(ntrajs-1), (phi_max(2) - phi_min(2)) / real(ntrajs-1)]
 
 !$OMP PARALLEL DO COLLAPSE(2) SCHEDULE(dynamic) PRIVATE(i, j, phi, phidot, H, N, slowroll, N_flush, k, u, filename, y) SHARED(phi_min, phi_max, dphi)
-do j = 1, 2
+do j = 1, ntrajs
 	do i = 1, ntrajs
 		! Background initial conditions
 		phi    = [phi_min(1) + real(i-1) * dphi(1), phi_min(2) + real(j-1) * dphi(2)] ! $\phi^{A}(t_{0})$
@@ -58,7 +58,8 @@ do j = 1, 2
 		write (filename, '("trajs/traj_", I0, ".txt")') k
 		open (unit=u, file=filename, status='replace', action='write')
 		
-		do while (N <= N_bound)
+		do while (slowroll <= 1.0 .or. abs(abs(phi(2))-2.5) >= 1.0d-3 .or. abs(phi(1)) >= 1.0d-3)
+!		do while (N <= N_bound)
 !		do while (abs(phi(1)) >= 0.05)
 			! Update functions of time
 			call unpack_state_background(y, phi, phidot, H, N)
@@ -67,7 +68,7 @@ do j = 1, 2
 			slowroll = - Hubbledot(phi, phidot) / H**2
 			
 			if (N >= N_flush) then
-				write (u,'(7(6e25.10e3))') phi, phidot, H, N
+				write (u,'(7(6e25.10e3))') phi, potential(phi), N
 				N_flush = N_flush + dN
 			end if
 			
