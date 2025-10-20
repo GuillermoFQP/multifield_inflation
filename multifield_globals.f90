b52d9fcc21551ac0cc95dc5983e018304bbc39d8
GuillermoFQP <gfq@sfu.ca>
2025-10-19 21:20:05 -0700

    Add/modify parameters

--- multifield_globals.f90
+++ multifield_globals.f90
@@ -3,8 +3,8 @@ module multifield_globals
 implicit none
 
 real, parameter             :: pi = 4.0 * atan(1.0)         ! Constant
-character(len=*), parameter :: field_space_geometry = "EUM" ! Choose between "EUM" for Euclidean, "RPM" for Renaux-Petel, "AAM" for alpha-attractor or "OWM" for oscillating warp metric
-character(len=*), parameter :: potential_shape = "NLP"      ! Choose between "ELP" for elliptic, "NLP" for non-linear, "HYP" for hybrid or "MVP" for modulated valley potential
+character(len=*), parameter :: field_space_geometry = "EUM" ! Choose between "EUM" for Euclidean, "RPM" for Renaux-Petel, "AAM" for alpha-attractor, "OWM" for oscillating warp metric, "GBM" for gaussian bumps metric
+character(len=*), parameter :: potential_shape = "DEP"      ! Choose between "ELP" for elliptic, "NLP" for non-linear, "HYP" for hybrid or "MVP" for modulated valley potential
 real, parameter             :: m1_ELP = 1.4d-6              ! Elliptic potential parameters
 real, parameter             :: m2_ELP = 7.0 * m1_ELP        ! Elliptic potential parameters
 real, parameter             :: lambda_NLP = 1.d-14          ! Non-linear potential parameters
@@ -16,10 +16,14 @@ real, parameter             :: m1_HYP = 0.3 * m2_HYP        ! Hybrid inflation p
 real, parameter             :: g_HYP = 0.8 * m2_HYP         ! Hybrid inflation potential parameters
 real, parameter             :: mu_HYP = 0.0                 ! Hybrid inflation potential parameters
 real, parameter             :: m_mvp = 1.4d-6               ! Modulated valley potential parameters
-real, parameter             :: lambda1_mvp = 2.0d-3         ! Modulated valley potential parameters
-real, parameter             :: f1_mvp = 1.0                 ! Modulated valley potential parameters
-real, parameter             :: f2_mvp = 5.0                 ! Modulated valley potential parameters
-real, parameter             :: f3_mvp = 2.0                 ! Modulated valley potential parameters
+real, parameter             :: lambda1_mvp = 2.00d-3        ! Modulated valley potential parameters
+real, parameter             :: f1_mvp = 1.00                ! Modulated valley potential parameters
+real, parameter             :: f2_mvp = 10.0                ! Modulated valley potential parameters
+real, parameter             :: f3_mvp = 0.50                ! Modulated valley potential parameters
+real, parameter             :: m1_DEP = 1.4d-6              ! Deformed elliptic potential parameters
+real, parameter             :: m2_DEP = 7.00 * m1_DEP       ! Deformed elliptic potential parameters
+real, parameter             :: amp_DEP = 5.00               ! Deformed elliptic potential parameters
+real, parameter             :: frq_DEP = 1.00               ! Deformed elliptic potential parameters
 real, parameter             :: alpha_aa = 1.0               ! $\alpha$-attractor metric parameters
 real, parameter             :: beta_aa = 1.0d10             ! $\alpha$-attractor metric parameters
 real, parameter             :: amp_OWM = 0.75               ! Oscillatory warp metric parameters
